d7ed4bd046721e43b1af31a0ddb264ba2559118a
Frank Harris <frank.harris@mytsp.net>
2024-10-19 21:20:15 -0500

    Updated Init

---
+++ Resources/StarSystemSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+	
+[CreateAssetMenu(fileName = "New StarSystemSO", menuName = "Star Systen Data", order = 51)]
+
+public class StarSystemSO : ScriptableObject
+{
+	[SerializeField]
+	private string systemName;
+	[SerializeField]
+	private int planetcount;
+	[SerializeField]
+	private Sprite starsprite;
+	[SerializeField]
+	[Tooltip
+		("Civilization can be SpaceFaring, Advanced, Warlike")]
+	private string civtype;
+	public int population;
+	
+	public string GetsystemName
+	{
+		get
+		{
+			return systemName;
+		}
+	}
+}
